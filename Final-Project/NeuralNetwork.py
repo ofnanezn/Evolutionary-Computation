8add6e41e398ed3553a009a378e31d1ee58f5815
ofnanezn <ofnanezn@unal.edu.co>
2018-10-06 22:32:17 -0500

    Hill Climbing with Neural Network Working

--- Final-Project/NeuralNetwork.py
+++ Final-Project/NeuralNetwork.py
@@ -1,13 +1,18 @@
 import numpy as np
+from copy import deepcopy
 
+"""
+	Functions required by the Neural Network
+"""
 def linear_forward(A_prev, W, b):
 	return np.dot(W, A_prev) + b
 
 def RELU(x):
-	return x if x > 0 else 0
+	return np.maximum(0, x)
 
 def sigmoid(x):
-	return 1/(1+np.exp(-x))
+	return 1/(1+np.exp(-x)) 
+
 
 class NeuralNetwork(object):
 	"""docstring for NeuralNetwork"""
@@ -27,17 +32,41 @@ class NeuralNetwork(object):
 	def forward_propagate(self, input_layer, W, b):
 		n_layers = len(W)
 		A = input_layer
-		for l in layers(n_layers):
+		for l in range(n_layers):
 			A_prev = A
 			Z = linear_forward(A_prev, W[l], b[l])
-			activation = self.activation_functions[l]
+			activation = eval(self.activation_functions[l])
 			A = activation(Z) 
 		return A
 
-	def cost(AL, Y):
+	def cost(self, AL, Y):
 		m = Y.shape[1]
 		cost = (-1 / m) * np.sum(np.multiply(Y, np.log(AL)) + np.multiply(1 - Y, np.log(1 - AL)))
 		return np.squeeze(cost)
+
+	def trainHillClimbing(self, X, W, b, Y, delta=0.6, num_iterations=1000):
+		history_loss = []
+		parameters = {}
+		for i in range(num_iterations):
+			AL = self.forward_propagate(X, W, b)
+			cost = self.cost(AL, Y)
+			W_prime = deepcopy(W)
+			b_prime = deepcopy(b)
+			for l in range(len(W_prime)):
+				W_prime[l] += delta * np.random.randn(W_prime[l].shape[0], W_prime[l].shape[1])
+				b_prime[l] += delta * np.random.randn(b_prime[l].shape[0], 1)
+			AL_prime = self.forward_propagate(X, W_prime, b_prime)
+			cost_prime = self.cost(AL_prime, Y)	
+			if cost_prime <= cost:
+				W = W_prime
+				b = b_prime
+				cost = cost_prime
+			history_loss.append(cost)
+		parameters["W"] = W
+		parameters["b"] = b
+		return parameters, history_loss
+
+	def evaluateModel(self, X, Y, parameters):
 
 
 
