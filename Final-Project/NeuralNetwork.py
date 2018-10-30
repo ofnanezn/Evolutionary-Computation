5eefcf76d22ed100a7044fe01233944cc84b81df
ofnanezn <ofnanezn@unal.edu.co>
2018-10-29 22:31:26 -0500

    Added mnist example

--- Final-Project/NeuralNetwork.py
+++ Final-Project/NeuralNetwork.py
@@ -34,7 +34,7 @@ class NeuralNetwork(object):
         b = []
         layers = [input_size] + self.hidden_layers + [output_size]
         for l in range(1, len(layers)):
-            W.append(np.random.randn(layers[l],layers[l-1]))
+            W.append(np.random.randn(layers[l],layers[l-1])*np.sqrt(1/(layers[l]+layers[l-1])))
             b.append(np.zeros((layers[l],1)))
         return W, b
 
@@ -74,7 +74,8 @@ class NeuralNetwork(object):
                     W_prime[l] += delta * powerLawGenerator(np.random.randn(W_prime[l].shape[0], W_prime[l].shape[1]))
                     b_prime[l] += delta * powerLawGenerator(np.random.randn(b_prime[l].shape[0], 1))
             AL_prime = self.forward_propagate(X, W_prime, b_prime)
-            cost_prime = self.cost(AL_prime, Y) 
+            cost_prime = self.cost(AL_prime, Y)
+            #print(i, cost, cost_prime)
             if cost_prime <= cost:
                 W = W_prime
                 b = b_prime
@@ -83,6 +84,8 @@ class NeuralNetwork(object):
         parameters["W"] = W
         parameters["b"] = b
         return parameters, history_loss
+
+
 
     def SimulatedAnnealingTrain(self, X, W, b, Y,
                                 t_max=1000, delta=0.6, powerLaw=True):
@@ -127,4 +130,91 @@ class NeuralNetwork(object):
         Y_predicted = np.argmax(Y_prime, axis=0)
         return accuracy_score(Y_true, Y_predicted)
 
+class ConvLayer():
+    def __init__(self, pad, f, slide, n_F):
+        """
+        pad -- Zero-padding size
+        f -- Filter size
+        """
+        self.pad = pad
+        self.f = f
+        self.slide = slide
+        self.n_F = n_F
 
+    def zero_pad(self, X):
+        #Pad with zeros all images of the dataset X
+        p = self.pad
+        X_pad = np.pad(X, ((0,0),(p,p),(p,p),(0,0)), 'constant')    
+        return X_pad
+
+    def conv_single_step(self, a_slice_prev, W, b):
+        """
+        Apply one filter defined by parameters W on a single slice (a_slice_prev) of the output activation 
+        of the previous layer.
+        """
+        s = a_slice_prev * W
+        # Sum over all entries of the volume s.
+        Z = np.sum(s)
+        # Add bias b to Z. Cast b to a float() so that Z results in a scalar value.
+        Z = Z + float(b)
+
+        return Z
+
+    def conv_forward(self, A_prev, W, b):
+        """
+        Implements the forward propagation for a convolution function
+        
+        Arguments:
+        A_prev -- output activations of the previous layer, numpy array of shape (m, n_H_prev, n_W_prev, n_C_prev)
+        W -- Weights, numpy array of shape (f, f, n_C_prev, n_C)
+        b -- Biases, numpy array of shape (1, 1, 1, n_C)
+        hparameters -- python dictionary containing "stride" and "pad"
+            
+        Returns:
+        Z -- conv output, numpy array of shape (m, n_H, n_W, n_C)
+        cache -- cache of values needed for the conv_backward() function
+        """
+    
+        # Retrieve dimensions from A_prev's shape (≈1 line)  
+        (m, n_H_prev, n_W_prev, n_C_prev) = A_prev.shape
+        
+        # Retrieve dimensions from W's shape (≈1 line)
+        (f, f, n_C_prev, n_C) = W.shape
+
+        # Retrieve information from "hparameters" (≈2 lines)
+        stride = self.stride
+        pad = self.pad
+        
+        # Compute the dimensions of the CONV output volume using the formula given above. Hint: use int() to floor. (≈2 lines)
+        n_H = int((n_H_prev - f + 2 * pad) / stride) + 1
+        n_W = int((n_W_prev - f + 2 * pad) / stride) + 1
+        
+        # Initialize the output volume Z with zeros. (≈1 line)
+        Z = np.zeros((m, n_H, n_W, n_C))
+        
+        # Create A_prev_pad by padding A_prev
+        A_prev_pad = zero_pad(A_prev, pad)
+        
+        for i in range(m):                                 # loop over the batch of training examples
+            a_prev_pad = A_prev_pad[i]                     # Select ith training example's padded activation
+            for h in range(n_H):                           # loop over vertical axis of the output volume
+                for w in range(n_W):                       # loop over horizontal axis of the output volume
+                    for c in range(n_C):                   # loop over channels (= #filters) of the output volume
+                        # Find the corners of the current "slice" (≈4 lines)
+                        vert_start = h * stride
+                        vert_end = vert_start + f
+                        horiz_start = w * stride
+                        horiz_end = horiz_start + f
+                        # Use the corners to define the (3D) slice of a_prev_pad (See Hint above the cell). (≈1 line)
+                        a_slice_prev = a_prev_pad[vert_start:vert_end, horiz_start:horiz_end, :]
+                        # Convolve the (3D) slice with the correct filter W and bias b, to get back one output neuron. (≈1 line)
+                        Z[i, h, w, c] = conv_single_step(a_slice_prev, W[...,c], b[...,c])
+                                            
+        # Making sure your output shape is correct
+        assert(Z.shape == (m, n_H, n_W, n_C))
+        
+        return Z
+
+    def initialize_weights(self, input):
+        n_C = input.shape[-1]
+        return np.random.randn(self.f, self.f, n_C, self.n_F)*np.sqrt(1/(layers[l]+layers[l-1]))
